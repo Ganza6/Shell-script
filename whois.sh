15e59da3d692bd31975614b40b25cbea00582153
Denis Koshelev <koden1998@mail.ru>
2018-11-04 01:49:51 +0300

    another one

--- whois.sh
+++ whois.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 function write(){
-	file_name=$(echo "$1" | tr "." "_")
-	whois="https://www.whois.com/whois/$1"
-	echo $whois
-	curl $whois > $2/$file_name.html
+	file_name=$(echo "$1" | tr ". / :" "_")
+	whois="https://www.whois.com/search.php?query=$1"
+	curl -L $whois > $2/$file_name.html # -L - поддержка редиректа
 }
 
 if [ ! $# == 3 ]; then
@@ -16,13 +15,9 @@ else
 		write $3 $1
 	elif [ $2 == "file" ]; then
 		while read domain; do
-			domain=${domain/www./ } #привожу строку к стандартному виду
 			domain=${domain%$'\r'}
-			domain=${domain/'http://'/ }
-			domain=${domain/'https://'/ }
 			write $domain $1
 		done < $3
-		write $domain $1
 	else
 		echo "Not domain, not file"
 		exit
